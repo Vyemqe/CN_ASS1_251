13e94b14230ba009438106cded407b613cb74e50
Jos <hohuyhungnbn@gmail.com>
2025-11-05 23:44:21 +0700

    start_proxy.py v2

--- start_proxy.py
+++ start_proxy.py
@@ -35,12 +35,12 @@ Requirements:
 
 """
 
-import socket
-import threading
+# import socket
+# import threading
 import argparse
 import re
 from urllib.parse import urlparse
-from collections import defaultdict
+from collections.abc import defaultdict
 
 from daemon import create_proxy
 
@@ -62,17 +62,17 @@ def parse_virtual_hosts(config_file):
     # host "<IP in here>" {<code block>}
     host_blocks = re.findall(r'host\s+"([^"]+)"\s*\{(.*?)\}', config_text, re.DOTALL)
 
-    dist_policy_map = ""
+    # dist_policy_map = ""
 
     routes = {}
     for host, block in host_blocks:
-        proxy_map = {}      # Dictionary {key: host, value: [proxy_pass]}
+        # proxy_map = {}      # Dictionary {key: host, value: [proxy_pass]}
 
         # Find all proxy_pass entries
         proxy_passes = re.findall(r'proxy_pass\s+http://([^\s;]+);', block)
-        map = proxy_map.get(host,[])
-        map = map + proxy_passes
-        proxy_map[host] = map
+        # map = proxy_map.get(host,[])
+        # map = map + proxy_passes
+        # proxy_map[host] = map
 
         # Find dist_policy if present
         policy_match = re.search(r'dist_policy\s+(\w+)', block)
@@ -89,16 +89,21 @@ def parse_virtual_hosts(config_file):
         #       the policy is applied to identify the highest matching
         #       proxy_pass
         #
-        if len(proxy_map.get(host,[])) == 1:    # In case of 1 proxy_pass
-            routes[host] = (proxy_map.get(host,[])[0], dist_policy_map)
+        # if len(proxy_map.get(host,[])) == 1:    # In case of 1 proxy_pass
+        #     routes[host] = (proxy_map.get(host,[])[0], dist_policy_map)
         # esle if:
         #         TODO:  apply further policy matching here
         #
-        else:
-            routes[host] = (proxy_map.get(host,[]), dist_policy_map)
-    print("Loaded virtual proxy routes:")
-    for host, (targets, policy) in routes.items():
-        print("{} -> {}, policy: {}".format(host, targets, policy))
+        # else:
+        #     routes[host] = (proxy_map.get(host,[]), dist_policy_map)
+        if (len(proxy_passes) == 1):        # 1 proxy
+            routes[host] = (proxy_passes[0], dist_policy_map)
+        else:                               # multiple proxies
+            routes[host] = (proxy_passes, dist_policy_map)
+    # print("Loaded virtual proxy routes:")
+    # for host, (targets, policy) in routes.items():
+    #     print("{} -> {}, policy: {}".format(host, targets, policy))
+    print("{} ({}, {})").format(host, routes[host], dist_policy_map)
     return routes
 
     # for key, value in routes.items():
@@ -106,28 +111,28 @@ def parse_virtual_hosts(config_file):
     # return routes
 
 ### UTILITIES ###
-def build_balancer(routes: dict) -> dict:
-    """
-    Builds load balancer structures to track current backend index for each host.
+# def build_balancer(routes: dict) -> dict:
+#     """
+#     Builds load balancer structures to track current backend index for each host.
 
-    :params routes (dict): dictionary mapping hostnames and location.
-    :rtype dict: mapping hostnames to their load balancer state.
-    """
-    balancer = {}
-    for host, (targets, policy) in routes.items():
-        if isinstance(targets, list) and policy == "round-robin":
-            balancer[host] = {
-                'targets': targets,
-                'index': 0,
-                'policy': policy
-            }
-        else:
-            balancer[host] = {
-                'targets': [targets],
-                'index': 0,
-                'policy': policy
-            }
-    return balancer
+#     :params routes (dict): dictionary mapping hostnames and location.
+#     :rtype dict: mapping hostnames to their load balancer state.
+#     """
+#     balancer = {}
+#     for host, (targets, policy) in routes.items():
+#         if isinstance(targets, list) and policy == "round-robin":
+#             balancer[host] = {
+#                 'targets': targets,
+#                 'index': 0,
+#                 'policy': policy
+#             }
+#         else:
+#             balancer[host] = {
+#                 'targets': [targets],
+#                 'index': 0,
+#                 'policy': policy
+#             }
+#     return balancer
 
 def get_next_backend(host, balancer):
     """
@@ -138,20 +143,31 @@ def get_next_backend(host, balancer):
     :rtype tuple: (backend_host, backend_port)
     """
     if host not in balancer:
-        return None, None
+        return None
 
-    entry = balancer[host]      # Get the balancer entry for the host
-    targets = entry['targets']
-    # curr_idx = entry['index'] # Don't need this, just update the actual index from entry, 
-                                # or else you need curr_idx=entry['index'] after entry['index']+=1
-    policy = entry['policy']
+    # entry = balancer[host]      # Get the balancer entry for the host
+    # targets = entry['targets']
+    # # curr_idx = entry['index'] # Don't need this, just update the actual index from entry, 
+    #                             # or else you need curr_idx=entry['index'] after entry['index']+=1
+    # policy = entry['policy']
 
-    if policy == "round-robin":
-        backend = targets[entry['index']]
-        entry['index'] = (entry['index'] + 1) % len(targets)  # Round-robin update
+    # if policy == "round-robin":
+    #     backend = targets[entry['index']]
+    #     entry['index'] = (entry['index'] + 1) % len(targets)  # Round-robin update
+    # else:
+    #     backend = targets[0]  # Default to first target if no policy matched
+    # return backend
+    targets, policy = balancer[host]    # tuple (target | [targets], policy)
+    if isinstance(targets, list) and policy == "round-robin":
+        index = getattr(get_next_backend, "index_map", {}).get(host, 0)
+        backend = targets[index]
+        newindex = (index + 1) % len(targets)  # Round-robin update
+        if not hasattr(get_next_backend, "index_map"):
+            get_next_backend.index_map = {}     # Custom attribute to store RR index per host
+        get_next_backend.index_map[host] = newindex
+        return backend
     else:
-        backend = targets[0]  # Default to first target if no policy matched
-    return backend
+        return targets  # single target
 
 if __name__ == "__main__":
     """
@@ -175,7 +191,7 @@ if __name__ == "__main__":
 
     #! 1. Parse config file
     routes = parse_virtual_hosts("config/proxy.conf")
-    #! 2. Build the balancer
-    balancer: dict = build_balancer(routes)
-    #! 3. Pass to create_proxy
-    create_proxy(ip, port, balancer)
+    # #! 2. Build the balancer
+    # balancer: dict = build_balancer(routes)
+    #! 2. Pass to create_proxy
+    create_proxy(ip, port, routes)
