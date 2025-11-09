d082f5524e39b7a9925f2b9c5eae366de90bf501
Jos <hohuyhungnbn@gmail.com>
2025-11-09 15:50:55 +0700

    Task 2 - v1

--- start_sampleapp.py
+++ start_sampleapp.py
@@ -29,6 +29,7 @@ and can be configured via command-line arguments.
 import argparse
 
 from daemon.weaprous import WeApRous
+from daemon.networkmode import NetworkManager   # network layer
 
 PORT = 8000  # Default port
 
@@ -63,27 +64,101 @@ def hello(headers, body):
 ### Add more endpoints here ###
 #! Client-Server
 #? 1. submit-info/ - PUT
+@app.routes('/submit-info', methods=['PUT'])
+def submit_info(headers, body):
+    """
+    Handle submission of user information: IP, port via PUT request.
+
+    This route processes the submitted user information and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing user information.
+    """
+    print("[SampleApp - C/S] ['PUT'] Submit info in {} to {}".format(headers, body))
 
 #? 2. add-list/ - POST
+@app.routes('/add-list', methods=['POST'])
+def add_list(headers, body):
+    """
+    Handle adding an item to a list via POST request.
+
+    This route processes the item to be added and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing the item to add.
+    """
+    print("[SampleApp - C/S] ['POST'] Add list in {} to {}".format(headers, body))
 
 #? 3. get-list/ - GET
+@app.routes('/get-list', methods = ['GET'])
+def get_list(headers, body):
+    """
+    Handle tracking list of peers via GET request.
+
+    This route processes the request to retrieve a list and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing any parameters for retrieval.
+    """
+    print("[SampleApp - C/S] ['GET'] Get peer list in {} to {}".format(headers, body))
 
 #? 4. connect/peer/ - POST
+@app.routes('/connect/peer', methods=['POST'])
+def connect_peer(headers, body):
+    """
+    Handle connecting to a peer via POST request.
+
+    This route processes the connection request and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing peer connection details.
+    """
+    print("[SampleApp - C/S] ['POST'] Connect peer in {} to {}".format(headers, body))
 
 #! P2P
 #? 1. broadcast-peer/ - POST
+@app.routes('/broadcast-peer', methods=['POST'])
+def broadcast_peer(headers, body):
+    """
+    Handle broadcasting peer information via POST request.
+
+    This route processes the broadcast request and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing peer information to broadcast.
+    """
+    print("[SampleApp - P2P] ['POST'] Broadcast peer in {} to {}".format(headers, body))
 
 #? 2. send-peer/ - PUT
+@app.routes('/send-peer', method = ['PUT'])
+def send_peer(headers, body):
+    """
+    Handle message exchanging between peers via PUT request.
+
+    This route processes the send request and prints it to the console.
+
+    :param headers (str): The request headers or user identifier.
+    :param body (str): The request body containing peer information to send.
+    """
+    print("[SampleApp - P2P] ['PUT'] Send peer in {} to {}".format(headers, body))
 
 if __name__ == "__main__":
     # Parse command-line arguments to configure server IP and port
     parser = argparse.ArgumentParser(prog='Backend', description='', epilog='Beckend daemon')
     parser.add_argument('--server-ip', default='0.0.0.0')
     parser.add_argument('--server-port', type=int, default=PORT)
+    #! For network layer
+    parser.add_argument('--mode', choices=['cs', 'p2p'], default='cs',
+                        help='Network mode: cs (Client-Server) or p2p (Peer-to-Peer).')
  
     args = parser.parse_args()
     ip = args.server_ip
     port = args.server_port
+    mode = args.mode
+    
+    #! Initialize network manager based on mode
+    network = NetworkManager(mode=mode, ip=ip, port=port)
+    network.setup_network()
 
     # Prepare and launch the RESTful application
     app.prepare_address(ip, port)
